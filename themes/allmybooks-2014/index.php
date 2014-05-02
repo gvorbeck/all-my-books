bce380d3f1388b60d2ab4a24496009a10b8eb826
Garrett <gvorbeck@gmail.com>
2014-05-02 17:58:48 -0400

    remove duplicates from table

--- themes/allmybooks-2014/index.php
+++ themes/allmybooks-2014/index.php
@@ -37,10 +37,34 @@ $args = array(
 		),
 	),
 );
-
 $future_query = new WP_Query( $args );
-
 if ( $future_query->have_posts() ) {
+	while ( $future_query->have_posts() ) {
+		$future_query->the_post();
+		global $wpdb;
+		$results = $wpdb->get_results( 'SELECT * FROM wp_reading_list WHERE bid = ' . $post->ID, ARRAY_N );
+		
+		echo '<h4>the count for ' . $post->ID . ' is ' . $results . '</h4>'; //REMOVE
+		var_dump($results); //REMOVE
+		
+		if ( count( $results ) > 1 ) {
+			// THERE ARE DUPLICATES OF THIS BOOK IN THE TABLE!!!
+			echo 'there are multiples'; //REMOVE
+			$i = 0;
+			foreach ($results as &$r) {
+				if ( $i > 0 ) {
+					$wpdb->delete( 'wp_reading_list', array( 'id' => $r[0] ) );
+				}
+				$i++;
+			}
+		}
+		if ( count( $results ) > 0 ) {
+			// THIS BOOK IS ALREADY PRESENT IN THE TABLE
+			echo 'there is something here';
+		}
+	}
+}
+/*if ( $future_query->have_posts() ) {
 	echo '<section id="future-read" class="book-shelf"><div class="ribbon"></div><h1>I Want To Read These</h1><ul id="future-read-list" class="book-list">';
 	while ( $future_query->have_posts() ) {
 		$future_query->the_post();
@@ -48,6 +72,6 @@ if ( $future_query->have_posts() ) {
 	}
 	echo '</ul></section>';
 }
-wp_reset_postdata();
+wp_reset_postdata();*/
 
 get_footer();
