2858b02f0b5c957b76e3f8a1da1fa399e70643f7
Garrett <gvorbeck@gmail.com>
2014-05-02 18:38:44 -0400

    future list fully built to read from a custom db table

--- themes/allmybooks-2014/index.php
+++ themes/allmybooks-2014/index.php
@@ -28,6 +28,7 @@ if ( $current_query->have_posts() ) {
 wp_reset_postdata();
 
 /* FUTURE READING */
+// 1. GET ALL POSTS MARKED AS 'WANT TO READ'.
 $args = array(
 	'posts_per_page' => -1,
 	'meta_query'     => array(
@@ -38,29 +39,27 @@ $args = array(
 	),
 );
 $future_query = new WP_Query( $args );
+// 2. GO THROUGH THEME AND FIND OUT IF THE 'READING LIST' TABLE HAS ANY PROBLEMS WITH THIS INFORMATION.
 if ( $future_query->have_posts() ) {
 	while ( $future_query->have_posts() ) {
 		$future_query->the_post();
 		$results = $wpdb->get_results( 'SELECT * FROM wp_reading_list WHERE bid = ' . $post->ID, ARRAY_N );
 		
-		echo '<h4>the count for ' . $post->ID . ' is ' . $results . '</h4>'; //REMOVE
-		var_dump($results); //REMOVE
-		
 		if ( count( $results ) > 1 ) {
 			// THERE ARE DUPLICATES OF THIS BOOK IN THE TABLE!!!
-			echo 'there are multiples'; //REMOVE
 			$i = 0;
 			foreach ($results as &$r) {
 				if ( $i > 0 ) {
+					// ...SO REMOVE THE DUPLICATES (BUT NOT THE ORIGINAL)
 					$wpdb->delete( 'wp_reading_list', array( 'id' => $r[0] ) );
 				}
 				$i++;
 			}
 		}
 		if ( count( $results ) > 0 ) {
-			// THIS BOOK IS ALREADY PRESENT IN THE TABLE
-			echo 'there is something here';
+			// THIS BOOK IS ALREADY PRESENT IN THE TABLE ...SO DO NOTHING.
 		} else {
+			// THIS BOOK IS NOT IN THE TABLE ...SO PUT IT IN THERE.
 			global $wpdb;
 			$bid           = $post->ID;
 			$table_name    = $wpdb->prefix . "reading_list";
@@ -69,14 +68,11 @@ if ( $future_query->have_posts() ) {
 	}
 }
 wp_reset_postdata();
-/*if ( $future_query->have_posts() ) {
-	echo '<section id="future-read" class="book-shelf"><div class="ribbon"></div><h1>I Want To Read These</h1><ul id="future-read-list" class="book-list">';
-	while ( $future_query->have_posts() ) {
-		$future_query->the_post();
-		the_book_builder( $post->ID );
-	}
-	echo '</ul></section>';
+// AT THIS POINT YOU HAVE CLEANED UP YOUR TABLE, NOW START LAYING IT  OUT IN CODE.
+$future_list = $wpdb->get_results( 'SELECT * FROM wp_reading_list', ARRAY_N );
+echo '<section id="future-read" class="book-shelf"><div class="ribbon"></div><h1>I Want To Read These</h1><ul id="future-read-list" class="book-list">';
+foreach ( $future_list as &$f ) {
+	the_book_builder( $f[2] );
 }
-wp_reset_postdata();*/
 
 get_footer();
