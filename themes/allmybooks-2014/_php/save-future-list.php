e816703858f63bae85a796783c6f8a09cf91978f
Garrett <gvorbeck@gmail.com>
2014-05-05 21:11:32 -0400

    fixed localhost ajax bug

--- themes/allmybooks-2014/_php/save-future-list.php
+++ themes/allmybooks-2014/_php/save-future-list.php
@@ -1,5 +1,11 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/wordpress/wp-config.php');
+if ( '::1' == $_SERVER["REMOTE_ADDR"] ) {
+	// IF THIS IS A LOCALHOST ENVIRONMENT
+	// IF YOU GET 500 INTERNAL SERVER ERRORS ON LOCAL HOST, EDIT THIS
+	require_once($_SERVER['DOCUMENT_ROOT'].'/allmybooks/wordpress/wp-config.php');
+} else {
+	require_once($_SERVER['DOCUMENT_ROOT'].'/wordpress/wp-config.php');
+}
 
 $future_list = $_POST['future_list'];
 
