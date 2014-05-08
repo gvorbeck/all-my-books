05e6a4298988e533d8cfabfff15cbe5d4bdd0ba2
Garrett <gvorbeck@gmail.com>
2014-05-08 19:29:51 -0400

    footer release notes

--- themes/allmybooks-2014/footer.php
+++ themes/allmybooks-2014/footer.php
@@ -6,7 +6,7 @@
 			<p>
 				<?php
 				echo file_get_contents( locate_template( "_images/icons/wordpress.svg" ) );
-				echo '<a target="_blank" href="https://github.com/gvorbeck/all-my-books" title="github repo">' . file_get_contents( locate_template( "_images/icons/github.svg" ) ) . ' Running AMB v1.1 (Bradbury)</a>';
+				echo '<a target="_blank" id="github-link" href="https://github.com/gvorbeck/all-my-books" title="github repo">' . file_get_contents( locate_template( "_images/icons/github.svg" ) ) . 'Running AMB v1.1 (Bradbury)</a>';
 				?>
 			</p>
     </footer>
