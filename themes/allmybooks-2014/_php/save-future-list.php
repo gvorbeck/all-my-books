4e0928966af2d118e7d29ee354956295190c72e2
Garrett <gvorbeck@gmail.com>
2014-05-08 18:33:29 -0400

    fixed dragging back to future list

--- themes/allmybooks-2014/_php/save-future-list.php
+++ themes/allmybooks-2014/_php/save-future-list.php
@@ -2,7 +2,7 @@
 
 // THIS IS COMMENTED OUT. CURRENTLY IT DOES NOT LOOK TO SEE IF LISTORDER COLUMN NUMBER IS ALREADY PRESENT SO YOU RESULT IN SEVERAL OF EACH LISTORDER NUMBER (EX: 1, 1, 1, 1, 2, 2, 2, 2, ETC ).
 
-/*if ( '::1' == $_SERVER["REMOTE_ADDR"] ) {
+if ( '::1' == $_SERVER["REMOTE_ADDR"] ) {
 	// IF THIS IS A LOCALHOST ENVIRONMENT
 	// IF YOU GET 500 INTERNAL SERVER ERRORS ON LOCAL HOST, EDIT THIS
 	require_once($_SERVER['DOCUMENT_ROOT'].'/allmybooks/wordpress/wp-config.php');
@@ -18,7 +18,7 @@ if ( ! empty( $future_list ) && is_user_logged_in() ) {
 		$pieces = explode( ':', $b );
 		update_post_meta( $pieces[0], 'reading_state', 2 );
 		$table_name = $wpdb->prefix . "reading_list";
-		$results     = $wpdb->get_results( 'SELECT * FROM wp_reading_list WHERE bid = ' . $post->ID, ARRAY_N );
+		$results     = $wpdb->get_results( 'SELECT * FROM wp_reading_list WHERE bid = ' . $pieces[0], ARRAY_N );
 		if ( count( $results ) > 0 ) {
 			$rows_affected = $wpdb->update(
 				$table_name,
@@ -36,4 +36,4 @@ if ( ! empty( $future_list ) && is_user_logged_in() ) {
 			);
 		}
 	}
-}*/
+}
