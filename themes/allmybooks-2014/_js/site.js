c20ba7abdc678665e851240cae134797e9748cbf
Garrett <gvorbeck@gmail.com>
2014-05-15 18:26:34 -0400

    added html5 sortable fucntionality

--- themes/allmybooks-2014/_js/site.js
+++ themes/allmybooks-2014/_js/site.js
@@ -78,37 +78,9 @@ jQuery( document ).ready( function() {
 		};
 	});
 	
-	// Set the two list's LIs as sortable.
-	jQuery( "#current-read-list, #future-read-list, #finished-read-list" ).sortable( {
-		update: function( event, ui ) {
-			if (this === ui.item.parent()[0]) { // Usually update fires for every list linked. Anything in here will only fire once.
-				//console.log(ui.item.attr('id')); // BOOK ID
-				//console.log(ui.item.parent()[0].id); // LIST ID
-				if ( 'future-read-list' == ui.item.parent()[0].id ) {
-					updateFutureList();
-				}
-				if ( 'current-read-list' == ui.item.parent()[0].id ) {
-					updateCurrentList(ui.item.attr('id'));
-				}
-				if ( 'finished-read-list' == ui.item.parent()[0].id ) {
-					updateFinishedList(ui.item.attr('id'));
-				}
-			}
-			//console.log(ui);
-		},
-		// Show Finished list.
-		activate: function( event, ui ) {
-			var clickedID = ui.item[0].id;
-			// This makes sure the finished list never shows unless I am moving a book from the currently-read list.
-			if ('current-read-list' == jQuery( '#' + clickedID ).parent().attr('id')){
-				jQuery( "#finished-read" ).slideDown( "slow", function() {
-				    // Animation complete.
-				} );
-			}
-		},
-		// Connect the two lists.
-  	connectWith: ".book-list"
-	} ).disableSelection();
+	jQuery('#finished-read-list, #current-read-list, #future-read-list').sortable({
+    connectWith: '.connected'
+	});
 	
 	// Show navigation/login menu.
 	jQuery( '.navigation--button, #navigation--popup').hover( function() {
