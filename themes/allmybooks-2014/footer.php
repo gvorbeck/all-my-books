06a8d04e1a4e8b26a73e5a3483c74586828e3a54
Garrett <gvorbeck@gmail.com>
2014-05-17 00:56:58 -0400

    footer fix

--- themes/allmybooks-2014/footer.php
+++ themes/allmybooks-2014/footer.php
@@ -3,7 +3,7 @@
     <footer id="site-footer">
 			<p id="copyright">&copy; <?php echo date('Y') . ' ' . get_bloginfo('name'); ?></p>
 			<p id="site-credit">Created by <a href="http://iamgarrett.com" title="iamgarrett">Garrett Vorbeck</a>.</p>
-			<p id="github-link"><?php echo '<a target="_blank" href="https://github.com/gvorbeck/all-my-books/releases/tag/v1.3" title="github repo"><span>all-my-books v1.3 (Dostoevsky)</span>'.trim(file_get_contents(locate_template("_images/icons/github.svg"))).'</a>';
+			<p id="github-link"><?php echo '<a target="_blank" href="https://github.com/gvorbeck/all-my-books/releases/tag/v1.3" title="github repo"><span>all-my-books v1.3.1 (Dostoevsky)</span>'.trim(file_get_contents(locate_template("_images/icons/github.svg"))).'</a>';
 				?>
 			</p>
     </footer>
