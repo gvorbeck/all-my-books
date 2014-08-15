0183541da2d4559c24ed2725ffd430c9b585c6e1
Garrett <gvorbeck@gmail.com>
2014-08-15 11:26:43 -0400

    footer data changes

--- themes/allmybooks-2014/footer.php
+++ themes/allmybooks-2014/footer.php
@@ -2,11 +2,12 @@
       <footer id="site-footer" class="content">
         <p id="copyright">&copy; <?php echo date('Y') . ' ' . get_bloginfo('name'); ?></p>
         <p id="site-credit">Created by <a href="http://iamgarrett.com" title="iamgarrett">Garrett Vorbeck</a>.</p>
-        <p id="github-link"><?php echo '<a target="_blank" href="https://github.com/gvorbeck/all-my-books/releases/tag/v1.5" title="github repo"><span>all-my-books v1.5 (Fitzgerald)</span>'. svg_github() .'</a>';
+        <?php $amb_theme = wp_get_theme(); ?>
+        <p id="github-link"><?php echo '<a target="_blank" href="https://github.com/gvorbeck/all-my-books/releases/latest" title="github repo"><span>' . $amb_theme->get( 'Name' ) . ' (' . $amb_theme->get( 'Version' ) . ')</span>'. svg_github() .'</a>';
           ?>
         </p>
       </footer>
-    </div> <!-- #container -->
+    </div><!-- #container -->
     <?php wp_footer(); ?>
   </body>
 </html>
