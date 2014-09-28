10f85d8874131f0d3509b3edebca09dbd907b0c6
Garrett <gvorbeck@gmail.com>
2014-09-28 19:40:22 -0400

    redesign

---
+++ themes/allmybooks-2014/index-finished_list.php
@@ -0,0 +1,7 @@
+<?php /* FINISHED READING */ ?>
+<section id="finished-read" class="book-shelf">
+  <h1><?php echo svg_bookmark(); ?>I'm Finished Reading This</h1>
+  <ul id="finished-read-list" class="book-list connected sortable">
+    <li class="instructions disabled">Drag a book here to mark it as finished.</li>
+  </ul>
+</section>
